170530da9e22a80d1272caa5d6d1dcfd57ccadb4
Mitko Mitev <onesible@gmail.com>
2024-12-06 21:10:23 +0200

    fix: added csrf_trusted_origins

--- book_nexus/settings.py
+++ book_nexus/settings.py
@@ -10,7 +10,11 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda v: v.split(',') if v else [])
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(',')
+
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', []).split(','))
+
+
 # Application definition
 
 INSTALLED_APPS = [
